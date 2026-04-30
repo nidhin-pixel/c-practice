9d188898f361694956d24d52656243464d439863
nidihin-pixel <nidhinagar2020@gmail.com>
2026-04-30 13:53:38 +0530

    unions

---
+++ unions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+union Data {
+    int i;
+    float f;
+    char str[20];
+} data;
+
+int main() {
+    data.i = 10;
+    printf("Integer value: %d\n", data.i);
+    data.f = 3.14;
+    printf("Float value: %f\n", data.f);
+    strcpy(data.str, "Hello, Union!");
+    printf("String value: %s\n", data.str);
+    return 0;
+}
